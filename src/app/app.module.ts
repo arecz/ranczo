87a652dc76e9bc305cec07989a546c1f650b4c18
Arkadiusz <nedar3@gmail.com>
2017-12-27 12:44:20 +0100

    add desktop flexbox layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { LandingComponent } from './landing/landing.component';
+import { LeftBarComponent } from './left-bar/left-bar.component';
+import { RightBarComponent } from './right-bar/right-bar.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    LandingComponent,
+    LeftBarComponent,
+    RightBarComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
