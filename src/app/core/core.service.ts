f1311c4c06f70263cae8fc6d2c5b1034a9f65cff
Arkadiusz <nedar3@gmail.com>
2018-01-23 12:53:22 +0100

    add hr text-spacing class, change text align to center, change offer buttons positions

--- src/app/core/core.service.ts
+++
@@ -1,7 +0,0 @@
-import { Injectable } from '@angular/core';
-import { ContentService } from '../content/content.service';
-@Injectable()
-
-export class CoreService {
-    constructor (private contentService: ContentService) {}
-}
